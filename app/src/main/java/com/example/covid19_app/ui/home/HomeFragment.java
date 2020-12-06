08a2bf1676b5f8728a9ffd82170fda55c7973904
evilmonkeydiaz <ilikebeans12345@hotmail.com>
2020-12-06 10:09:50 -0800

    forgot to add these

--- app/src/main/java/com/example/covid19_app/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/covid19_app/ui/home/HomeFragment.java
@@ -1,12 +1,13 @@
 package com.example.covid19_app.ui.home;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.TextView;
+
+
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,61 +15,93 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 import com.example.covid19_app.R;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import java.util.Map;
+import java.util.List;
+import java.util.HashMap;
 
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+import android.widget.SimpleAdapter;
+
+import android.widget.Toast;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
-    private TextView mTotalConfirmed;
 
-            private TextView textView1;
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_current_location, container, false);
-        mTotalConfirmed = root.findViewById(R.id.tvTotalConfirmed);
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        homeViewModel =
+                new ViewModelProvider(this).get(HomeViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
 
+        //final TextView textView = root.findViewById(R.id.text_home);
+        final TextView totalView = root.findViewById(R.id.totalCasesForCurrentLocation);
 
-       // textView1 = root.findViewById(R.id.tvTotalConfirmed);
-        final TextView textView = root.findViewById(R.id.text_home);
+//        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
 
+//        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                totalView.setText("Hey there");
+//            }
+//        });
+        totalView.setText("Hey there");
+//
+//        //root = inflater.inflate(R.layout.fragment_home, container, false);
+//        String[] menuItems = {"Do something", "dfjsdfkljsdf", "sdfkjhsdfsdfh", "sdfkjhsdfsdfh", "sdfkjhsdfsdfh", "sdfkjhsdfsdfh", "sdfkjhsdfsdfh", "sdfkjhsdfsdfh", "sdfkjhsdfsdfh", "sdfkjhsdfsdfh", "sdfkjhsdfsdfh", "sdfkjhsdfsdfh", "sdfkjhsdfsdfh", "sdfkjhsdfsdfh", "sdfkjhsdfsdfh", "sdfkjhsdfsdfh", "sdfkjhsdfsdfh", "sdfkjhsdfsdfh", "sdfkjhsdfsdfh", "sdfkjhsdfsdfh"};
+//
+//        ListView listView = root.findViewById(R.id.listView);
+//
+//        ArrayAdapter<String> listViewAdapter = new ArrayAdapter<String>(
+//                getActivity(),
+//                android.R.layout.simple_list_item_1,
+//                menuItems
+//        );
+//
+//        listView.setAdapter(listViewAdapter);
 
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        String[] titleArray = {"State", "fdssdf", "title 3", "title 4"};
+        String[] detailArray = {"detail 1", "detail 2", "detail 3", "detail 4"};
 
-        getActivity().setTitle("Overview");
+        List<Map<String, String>> listArray = new ArrayList<>();
 
-        // call Volley
+        for(int i=0; i< titleArray.length; i++)
+        {
+            Map<String, String> listItem = new HashMap<>();
+            listItem.put("titleKey", titleArray[i]);
+            listItem.put("detailKey", detailArray[i]);
+            listArray.add(listItem);
+        }
 
+        SimpleAdapter simpleAdapter = new SimpleAdapter(getActivity(), listArray,
+                android.R.layout.simple_list_item_2,
+                new String[] {"titleKey", "detailKey" },
+                new int[] {android.R.id.text1, android.R.id.text2 });
+
+        ListView listView = root.findViewById(R.id.listView);
+        listView.setAdapter(simpleAdapter);
 
         return root;
     }
-
-    private String getDate(long milliSecond){
-        // Mon, 23 Mar 2020 02:01:04 PM
-        SimpleDateFormat formatter = new SimpleDateFormat("EEE, dd MMM yyyy hh:mm:ss aaa");
-
-        Calendar calendar= Calendar.getInstance();
-        calendar.setTimeInMillis(milliSecond);
-        return formatter.format(calendar.getTime());
-    }
-
 
 }
